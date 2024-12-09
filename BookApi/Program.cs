a28593245d2016e8e792f2281581e6d7dc6ac652
Lmdingi <lmdingi4@gmail.com>
2024-12-09 10:54:07 +0200

    Add Controllers

---
+++ BookApi/Program.cs
@@ -0,0 +1,22 @@
+namespace BookApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            app.MapControllers();
+            app.MapGet("/", () =>
+            {
+                return Results.Redirect("/api/books");
+            });
+
+            app.Run();
+        }
+    }
+}
